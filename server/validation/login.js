e6113c9dfa8d4ba699f7ca2349cb66c686a0999f
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2020-01-22 17:18:11 -0500

    name fields, email, and password fields

---
+++ server/validation/login.js
@@ -0,0 +1,28 @@
+const validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateLoginInput(data) {  //exporting the function validateLoginInput
+  let errors = {};
+
+  //Convert any empty fields to an empty string so we can use validator
+  functions
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+
+  //Email checks
+    if (validator.isEmpty(data.email)) {    //delievers the error message if user doesn't type anything
+      errors.email = "Email field is required";
+    } else if (!validator.isEmail(data.email)) { //the validator from the npm library checks to make sure it is a valid form of email
+      errors.email = "Email is invalid";
+    }
+
+  //Password checks
+    if (validator.isEmpty(data.password)) { //delievers the error message if user doesn't type anything
+      errors.password = "Password field is required";
+    }
+
+  return (
+    errors,
+    isValid: isEmpty(errors)
+  );
+};
