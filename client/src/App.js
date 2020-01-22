5fbac13bda793cd2d26d6c208be8b505d062cb8c
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2020-01-21 21:54:53 -0500

    Setting up pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
+import Class from './Class';
+import Login from './Login';
+import Mainpage from './Mainpage';
+import Signup from './Signup';
+import Nav from './Nav';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Testing!
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className = "App">
+        <Nav />
+
+        <Route path = "/Class" component = {Class}/>
+        <Route path = "/Login" component = {Login}/>
+        <Route path = "/Mainpage" component = {Mainpage}/>
+        <Route path = "/Signup" component = {Signup}/>
+
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Nav(){
+    return(
+        <nav>
+            <h3>Grade Wizard</h3>
+            <ul className = "navlink">
+                <li>Mainpage</li>
+            </ul>
+        </nav>
+            
+    )
+}
+
+export default Nav;
