16f53235c6d2932c2a6caed6d709318398cac4db
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2020-01-23 13:16:43 -0500

    Login endpoint

--- server/api/users.js
+++ server/api/users.js
@@ -51,3 +51,60 @@ User.findOne({ email: req.body.email }).then(user => {  //a method of MongoDB th
     }
   });
 });
+
+
+// @route POST login/../api/users
+// @desc Login user and return JWT token
+// @access Public
+router.post("../validation/login", (req, res) => {
+  //form validation
+
+const { errors, isValid } = validateLoginInput(req.body); //pull the errors and isValid vars from the validateLogInput func and checks input validation
+
+//Check validation
+  if (!isValid) {
+    return res.status(400).json(errors); //throws the "bad request" error
+  }
+
+const email = req.body.email;     //stores entered email
+  const password = req.body.password;   //stores entered password
+
+// Find user by Email
+  User.findOne({ email }).then(user => {   //if email entered was a valid input, run it through this block
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found, 404 dumbass"}); //if user email does not exist throw error message
+    }
+
+// Check password
+    bcrypt.compare(password, user.password).then(isMatch => {   //uses the bcrypt library functions to check if information matches what is in our data
+      if (isMatch) {
+        // User Matched
+        // Create JWT payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+// Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,   //from the configure.js file
+          {
+            expiresIn: 86400 // 1 day in seconds, the amunt of time we're providing per session for users
+          },
+          (err, token) => {   //if correct append to a bearer string
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+        .status(400)
+        .json({ passwordincorrect: "Password Incorrect. Smh no wonder you need a grade calculator"});
+      }
+    });
+  });
+});
