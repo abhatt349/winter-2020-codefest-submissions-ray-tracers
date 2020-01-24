77657147666795eefb60c3b30e2de478b3ee9be0
Andy Mina <andy.mina@macaulay.cuny.edu>
2020-01-24 16:42:52 -0500

    Restyled login/signup and fixed folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,20 @@
 import React from 'react';
 import './App.css';
-import Class from './Class';
-import Login from './Login';
-import Home from './Home';
-import Signup from './Signup';
-import Nav from './Nav';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {Link} from 'react-router-dom';
+import screens from './comp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store';
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
         <Router>
-          <div className="App">
-            <Nav/>
-            <Switch>
-              <Route
-                exact path="/class"
-                component={Class}/>
-              <Route
-                exact path="/login"
-                component={Login}/>
-              <Route
-                exact path="/home"
-                component={Home}/>
-              <Route
-                exact path="/signup"
-                component={Signup}/>
-            </Switch>
-          </div>
+          <Switch>
+            <Route exact path="/" component={screens.Login}/>
+            <Route exact path="/login" component={screens.Login}/>
+            <Route exact path="/signup" component={screens.Signup}/>
+          </Switch>
         </Router>
-      </Provider>
     );
   }
 }
