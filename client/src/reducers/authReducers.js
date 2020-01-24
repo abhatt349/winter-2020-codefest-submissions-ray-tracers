24278f8ff7a3779c84dafc37953076d7a725fb21
kushendra1 <kushendra.ramrup59@myhunter.cuny.edu>
2020-01-24 16:49:54 -0500

    Added Redux

---
+++ client/src/reducers/authReducers.js
@@ -0,0 +1,30 @@
+// import all our actions from our types.js file
+import {  SET_CURRENT_USER, USER_LOADING } from "../actions/types";
+
+const isEmpty = require("is-empty");
+
+// define the initial state
+const initialState = {
+  isAuthenticated: false,
+  user: {},
+  loading: false
+};
+
+export default function(state = intialState, action) {
+  // Define how the state should change based on actions using a switch statement
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
+    case USER_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+}
---
+++ client/src/store.js
@@ -0,0 +1,21 @@
+//importing dependencies
+import (createStore, applyMiddleware, compose) from "redux";
+import thunk from "reux-thunk";
+import rootReducer from "./reducers";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+//creating a Redux store that holds the complete status tree of the app
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),           //applying all the middleware we have coded beforehand
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&    //used for debugging
+window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
